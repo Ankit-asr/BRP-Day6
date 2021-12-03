f3591ccf00ae1a6778b8e0aafb9f452e21af6a42
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-12-04 00:06:53 +0530

    [Ankit]Add : Encapsulation by Get and Set Properties

---
+++ BRP-Day6/Encapsulation/TestCircle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    public class StudentEncapsulation
+    {
+        private String studentName;
+        private int studentAge;
+        public String Name
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                studentName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return studentAge;
+            }
+            set
+            {
+                studentAge = value;
+            }
+        }
+    }
+    class GFG
+    {
+        static public void Main()
+        {
+            StudentEncapsulation obj = new StudentEncapsulation();
+            obj.Name = "Ankit";
+            obj.Age = 28;
+            Console.WriteLine("Name: " + obj.Name);
+            Console.WriteLine("Age: " + obj.Age);
+        }
+    }
+}
